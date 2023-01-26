6ef68c0db911ab69a0b9902be91f1ad11a2b7929
ALOSER <2878993617@qq.com>
2023-01-26 08:56:45 +0800

    更改目录结构

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"tiktok/routers"
+	"tiktok/web"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	routers.RouterInit(r)
+	web.RouterInit(r)
 	r.Run() //127.0.0.1:8080
 }
